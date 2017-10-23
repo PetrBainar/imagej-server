3ebe2deee86889b7be7091e3b6bd967ee70f911a
Unknown <petr.bainar@hotmail.com>
2017-10-23 15:07:23 +0200

    react-client-setup: adding jquery and implementing a connection check

--- clients/webNew/src/main.js
+++ clients/webNew/src/main.js
@@ -1,7 +1,39 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var $ = require('jquery');
+
+// Consts
+const HOST = 'http://localhost:8080';
+const DIR_MODULES = '/modules';
+const JSX_SUCCESS = (<h1>Success</h1>);
+const JSX_FAILURE = (<h1>Not yet</h1>);
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}	
+	
+	render() {
+		let retrievalSuccess = this.checkConnectionJQuery();
+		return retrievalSuccess ? JSX_SUCCESS : JSX_FAILURE;
+	}
+
+	checkConnectionJQuery() {		
+		$.ajaxSetup({
+			async: false
+		});
+		
+		let success = false;
+		
+		$.getJSON(HOST + DIR_MODULES, function(data, status) {
+			success = true;
+		});
+		
+		return success;		
+	}
+}
 
 ReactDOM.render(
-  <h1>New Fiji Web Client</h1>,
+  <App />,
   document.getElementById('app')
 );
